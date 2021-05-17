16db404c77f443b1d3f6af96169f25e059ecbd36
Ifeanyi Nwodo <joshuasearch@gmail.com>
2021-05-17 11:45:45 +0100

    Implemented CRUD

---
+++ CRUD-BackendAPI/CRUD-BackendAPI/Models/PaymentDetailDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_BackendAPI.Models
+{
+    public class PaymentDetailDBContext:DbContext
+    {
+        public PaymentDetailDBContext(DbContextOptions<PaymentDetailDBContext> options) : base(options){
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
